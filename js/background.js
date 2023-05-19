d61ab990f978e56ae890408f3cc27cb615c56514
ThioKDG <kim2006207@gmail.com>
2023-05-19 12:37:02 +0900

    0519 todo 삭제버튼 제작중

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
 const body = document.body; /* dom에서 body부분 끌어오기 */
 
 async function getPixa() { /* API 호출 및 배경그림 랜덤 부여 */
-    const imgGet = await fetch(`https://pixabay.com/api/?key=${KEY}&q=morning&image_type=photo`);
+    const imgGet = await fetch(`https://pixabay.com/api/?key=${KEY}&q=sky&image_type=photo`);
     const imgList = await imgGet.json();
     const bgList = imgList.hits[Math.floor(Math.random() * imgList.hits.length)].largeImageURL; /* hit.s */
     // console.log(results[Math.floor(Math.random() * results.length)]); 띄워쓰니깐 되네?
--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,8 @@ const todoList = document.getElementById('todo-list');
 
 const TODOS_KEY = 'todos'
 
-const toDos = [];
+let toDos = []; // 1 todo 배열은 시작할때마다 항상 비어있어 배열안이 덮어씌워지는 현상이 발생함
+/* 상수에서 변수값으로 변경시켜 업데이트를 할 수 있도록 바꾼다. */
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -17,13 +18,14 @@ function deleteToDo(e) {
 
 function paintToDo(newTodo) {
     const list = document.createElement('li');
+    list.id = newTodo.id;
     const listToSpan = document.createElement('span');
     const listButton = document.createElement('button');
     listButton.innerText = '❌';
     listButton.addEventListener('click', deleteToDo)
     list.appendChild(listToSpan);
     list.appendChild(listButton);
-    listToSpan.innerText = newTodo;
+    listToSpan.innerText = newTodo.text;
     todoList.appendChild(list);
 }
 
@@ -31,20 +33,20 @@ function handleToDoSubmit(e) {
     e.preventDefault();
     const newToDo = todoInput.value;
     todoInput.value = '';
-    toDos.push(newToDo);
-    paintToDo(newToDo);    
+    const newToDoObj = {
+        id: Date.now(),
+        text : newToDo, 
+    }
+    toDos.push(newToDoObj); // 1 그래서 계속 newtodo를 작성하고 form에 submit 할때 빈배열에 push를 하게 됨
+    paintToDo(newToDoObj);    
     saveToDos();
 }
 
 todoForm.addEventListener('submit', handleToDoSubmit);
 
-function sayHello(){
-    console.log('hello');
-}
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
-console.log(savedToDos);
 if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach(sayHelo);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
